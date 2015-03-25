a1e78c5a3f81b54abd2667e129435318bc26b58d
Erik Veld <erik.veld@lable.nl>
2015-03-25 15:38:08 +0100

    Added some documentation

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 /* jshint node: true */
 'use strict';
 
+// For testing purposes get the auth from command line, eventually get it from the bookmaker.
 var auth = process.argv[2];
 
 var bookkeeper = require('./bookkeeper.js');
 var archivist = require('./archivist.js');
 
+// Get the settings from the bookmaker.
 bookkeeper.getSettings('archivist', function gotSettings(settings) {
 	settings.auth = auth;
 	archivist.configure(settings);
 	archivist.connect();
 });
 
+// When we receive a message (in the future) archive it.
 archivist.on('message', function(message) {
 	console.log('%s %s -> %s: %s', message.timestamp, message.channel, message.user, message.message);
 });
 
+// Act on chosen or dropped channels.
 bookkeeper.on('joined', archivist.joinChannel);
 bookkeeper.on('parted', archivist.leaveChannel);
